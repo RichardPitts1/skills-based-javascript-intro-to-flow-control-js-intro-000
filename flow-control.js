4ba41d4c419cbf8c462b29edc6914fc490f518a4
Richard Pitts <richardepitts@gmail.com>
2019-08-04 13:59:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,8 +18,40 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!"
+  } else if (age <= 13 {
+    return "You are not a teenager!";
+  }
 }
+
+
+
+
+
+  describe('ageChecker', () => {
+    it('should return "You are a teenager!" if the age is between 13-19', () => {
+      expect(ageChecker(13)).toEqual("You are a teenager!")
+    })
+
+    it('should return "You are a kid" if the age is 12 or below', () => {
+      expect(ageChecker(12)).toEqual("You are a kid")
+    })
+
+    it('should return "You are a grownup" if the age is 20 or above', () => {
+      expect(ageChecker(29)).toEqual("You are a grownup")
+    })
+  })
+
+
+
+
+
+
+
+
+
+
 
 function ternaryTeenager(age) {
 
