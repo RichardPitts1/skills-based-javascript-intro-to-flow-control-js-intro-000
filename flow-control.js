8c7ba939f7bc1b848871da5c8448feb397e9cea2
Richard Pitts <richardepitts@gmail.com>
2019-08-04 14:12:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,7 +35,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
+  switch (age) {
     case (age >= 13 && age <= 19):
       return "You are a teenager";
     case (age < 13 || age > 19): 
