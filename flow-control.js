ff67c42f9998c4bff9e42b561f42e19aa202c6ed
Richard Pitts <richardepitts@gmail.com>
2019-08-04 13:57:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,11 @@ function basicTeenager(age) {
 // console.log(basicTeenager(20))
 // console.log(basicTeenager(12))
 function teenager(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager!";
+  }
 }
 
 function ageChecker(age) {
